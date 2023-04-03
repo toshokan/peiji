c41ceb9edcf7684dad8304c9b8a4150a628abd80
toshokan <toshokan@shojigate.net>
2023-04-03 12:01:36 -0400

    feat: use unpack for lua module args

--- lua/peiji.lua
+++ lua/peiji.lua
@@ -9,16 +9,28 @@
 -- args[4] = new charge amount
 -- args[5] = quick block timeout
 -- args[6] = repeat block timeout
+--
+-- Returns:
+-- { is_blocked, charge_success, current_count, block_seconds }
+-- 
 local function charge_bucket(keys, args)
-  local is_blocked = redis.call('GET', keys[2])
+  local bucket_name, blocked_bucket_name = unpack(keys)
+  local window_begin_ts,
+        window_max_quota,
+        new_charge_id,
+        new_charge_amt,
+        quick_block_timeout,
+        repeat_block_timeout = unpack(args)
+        
+  local is_blocked = redis.call('GET', blocked_bucket_name)
   if is_blocked
   then
-    redis.call('SETEX', keys[2], args[6], 1)
-    return {true, false, 0}
+    redis.call('SETEX', blocked_bucket_name, repeat_block_timeout, 1)
+    return {true, false, 0, repeat_block_timeout}
   end
 
   local total = 0
-  local in_range = redis.call('XRANGE', keys[1], args[1], '+')
+  local in_range = redis.call('XRANGE', bucket_name, window_begin_ts, '+')
   for i,v in ipairs(in_range)
   do
     local fields = v[2]
@@ -31,14 +43,16 @@ local function charge_bucket(keys, args)
     end
   end
 
-  local new_total = total + args[4]
-  if new_total <= tonumber(args[2])
+  local new_total = total + new_charge_amt
+  local max_quota_num = tonumber(window_max_quota)
+  
+  if new_total <= max_quota_num
   then
-    redis.call('XADD', keys[1], args[3], "src", "peiji", "cost", args[4])
-    return {false, true, new_total}
+    redis.call('XADD', bucket_name, new_charge_id, "src", "peiji", "cost", new_charge_amt)
+    return {false, true, new_total, 0}
   else
-    redis.call('SETEX', keys[2], args[5], 1)
-    return {true, false, total}
+    redis.call('SETEX', blocked_bucket_name, quick_block_timeout, 1)
+    return {true, false, total, quick_block_timeout}
   end
 end
 
